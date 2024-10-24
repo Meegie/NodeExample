3243a2499c4696f436b984cdf8f11df364a3cdd4
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-10-24 18:07:07 +0000

    Make more efficient

--- BaseCommands.sh
+++ BaseCommands.sh
@@ -3,7 +3,17 @@ apt update -y
 apt upgrade -y
 
 # Required
-apt install curl wget sudo -y
+apt install curl wget sudo git -y
 
 # NVM
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 20
+
+mkdir app
+cd app
+git clone https://github.com/Meegie/NodeExample .
+npm install
